4d4b1a0e721d18288ad2cf46a8416a8de12c41fe
brian <brian.tu@telexpress.com>
2024-01-28 21:25:00 +0800

    feat: initial

---
+++ index.ts
@@ -0,0 +1,124 @@
+class Node {
+  public id: any
+  public parentId: any
+  public children: Array<Node> | null | undefined
+
+  /**
+   * 與 root 的距離
+   */
+  public height: number
+  constructor(id: any, parentId: any, children: Array<Node> | null | undefined, parameters?: object) {
+    this.id = id
+    this.parentId = parentId
+    this.children = children
+    this.height = 0
+
+    if (parameters) {
+      for (const key in parameters)
+        this[key as keyof typeof this] = parameters[key as keyof typeof parameters]
+    }
+  }
+}
+
+class Tree {
+  public root: Node
+
+  public height: number
+
+  constructor(root: Node) {
+    this.root = root
+    this.height = 0
+  }
+
+  /**
+   * 取得特定節點
+   * @param {string} id node 的 id
+   * @returns {Node} 節點
+   */
+  public find(id: string): Node | null {
+    const result = this.findNode(id, this.root)
+
+    return result
+  }
+
+  /**
+   * 取得特定分支度
+   * @param {string} id 節點 id
+   * @returns {number} 分支度
+   */
+  public getDegree(id: string): number {
+    const node = this.find(id)
+    if (node && node.children)
+      return node.children.length
+
+    return 0
+  }
+
+  /**
+   * 搜尋節點
+   * @param id {string} id node 的 id
+   * @param node {Node} 節點
+   * @returns {(Node|null)} 節點
+   */
+  private findNode(id: string, currntNode: Node): Node | null {
+    if (currntNode.id === id)
+
+      return currntNode
+
+    if (currntNode.children && currntNode.children.length > 0) {
+      for (let i = 0; i < currntNode.children.length; i++) {
+        if (currntNode.children[i]) {
+          const result = this.findNode(id, currntNode.children[i])
+          if (result)
+            return result
+        }
+      }
+    }
+
+    return null
+  }
+
+  /**
+   * 建立樹
+   * @param {Array<Node>} nodes 節點
+   * @returns {Tree} 樹
+   */
+  static build(nodes: Array<Node>): Tree | null | undefined {
+    const root = nodes.find(node => node.id === null)
+
+    if (root === null)
+      return null
+
+    const rootHeight = 0
+    const heightArray: Array<number> = [rootHeight]
+
+    const getMaxHeight = (height: Array<number>): number => {
+      return Math.max(...height)
+    }
+
+    const getChildren = (node: Node, all: Array<Node>, parentHeight: number): void => {
+      const currentHeight = parentHeight + 1
+      const children = all.filter(item => item.parentId === node.id)
+
+      if (children.length <= 0)
+        return
+
+      for (const child of children) {
+        child.height = currentHeight
+        heightArray.push(currentHeight)
+        getChildren(child, all, currentHeight)
+      }
+
+      node.children = children
+    }
+
+    getChildren(root!, nodes, rootHeight)
+
+    const tree = new Tree(root!)
+    tree.height = getMaxHeight(heightArray)
+
+    return tree
+  }
+}
+
+export { Node, Tree }
---
+++ test.js
@@ -0,0 +1,12 @@
+import categories from './data/product-category';
+import { Node, Tree } from './index';
+const datas = [];
+for (const item of categories)
+    datas.push(new Node(item.id, item.parentCategoryId, null, item));
+const tree = Tree.build(datas);
+const id = '08dbba56-36bd-4832-8be6-d61a4e5d89ad';
+// const node = tree?.find(id)
+// eslint-disable-next-line no-console
+console.log(tree);
+// eslint-disable-next-line no-console
+console.log(`分支度:${tree?.getDegree(id)}`);
