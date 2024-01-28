ee0ff29edab004289fa80c36925be81ddc4fd686
brian <ay83103@gmail.com>
2024-01-28 23:07:28 +0800

    complete

--- test.ts
+++ test.ts
@@ -1,19 +1,23 @@
-import categories from './data/product-category'
-import { Node, Tree } from './index'
+import categories from './data/product-category.js'
+import { Node, Tree } from './index.js'
 
-const datas: Array<Node> = []
+const allNodes: Array<Node> = []
 
 for (const item of categories)
-  datas.push(new Node(item.id, item.parentCategoryId, null, item))
+  allNodes.push(new Node(item.id, item.parentCategoryId, [], item))
 
-const tree = Tree.build(datas)
+// const tree = Tree.build(allNodes)
 
-const id = '08dbba56-36bd-4832-8be6-d61a4e5d89ad'
+// const id = '08dbba56-36bd-4832-8be6-d61a4e5d89ad'
 
 // const node = tree?.find(id)
 
-// eslint-disable-next-line no-console
-console.log(tree)
+// console.log(tree)
 
+// console.log(`分支度:${tree?.getDegree(id)}`)
+
+const leaves = allNodes.filter(leaf => leaf.id === '08dbba56-36bd-4832-8be6-d61a4e5d89ad' || leaf.id === 'b5a26ae1-3966-4d03-9072-a694753458c1')
+
+const treeFromLeaves = Tree.buildFromLeaves(leaves, allNodes)
 // eslint-disable-next-line no-console
-console.log(`分支度:${tree?.getDegree(id)}`)
+console.log(treeFromLeaves)
