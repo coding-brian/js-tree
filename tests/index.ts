45eb92ca2aa0c302b3b4c30dd14e6cbf421f999f
brian <brian.tu@telexpress.com>
2024-03-01 17:54:49 +0800

    feat: fix test

--- tests/index.ts
+++ tests/index.ts
@@ -1,3 +1,22 @@
-it('測試', () => {
-  expect(1).toBe(1)
+import mockNodes from '../data/nodes.ts'
+import { Node, build } from '../src/index.ts'
+import type { Tree } from '../src/index.ts'
+
+const datas: Array<Node> = []
+
+for (const item of mockNodes)
+  datas.push(new Node(item.id, item.parentCategoryId, null, item))
+
+function initialTree(nodes: Array<Node>): Tree | null | undefined {
+  return build(nodes)
+}
+
+it('addNode', () => {
+  const node = new Node('4', null, null)
+  const tree = initialTree(datas)
+
+  tree?.addNode(node)
+  const target = tree?.find(node.id)
+  
+  expect(target?.id).toBe(node.id)
 })
